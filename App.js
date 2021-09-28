ad65be75b508d6f22a2859ae94a4b956de34c6e7
ShoaibTamboli <shoaibtamboli03@gmail.com>
2021-09-29 01:25:57 +0530

    update

--- App.js
+++ App.js
@@ -5,3 +5,20 @@ const checkNumberButton = document.querySelector("#check");
 checkNumberButton.addEventListener('click',function getValues(){
   console.log(dateOfBirth.value,luckyNumber.value);
 })
+function checkBirthDateIsLucky(){
+    const dob =dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum);
+  }
+  
+  function calculateSum(dob) {
+    dob= dob.replaceAll("-","");
+    let sum =0;
+    for(let index=0;index<dob.length;index++){
+      sum = sum + Number(dob.charAt(index));
+    }
+    return sum;
+  }
+  
+  checkNumberButton.addEventListener('click',checkBirthDateIsLucky)
+  
